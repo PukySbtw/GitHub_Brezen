01fac0d8c2b8dbe291af34a935bbc1c9e50df2e7
Lukáš <Luk@DESKTOP-AFSS14U>
2022-04-25 19:21:47 +0200

    Přidejte soubory projektu.

---
+++ ProjektBrezen/Program.cs
@@ -0,0 +1,14 @@
+﻿string input1 = Console.ReadLine();
+string input2 = Console.ReadLine();
+
+int cislo1 = Convert.ToInt32(input1); 
+int cislo2 = Convert.ToInt32(input2);
+
+if (cislo1 > cislo2)
+{
+    Console.WriteLine($"{cislo1}, {cislo2}");
+}
+else if (cislo1 < cislo2)
+{
+    Console.WriteLine($"{cislo2}, {cislo1}");
+}
